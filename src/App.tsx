df153fae785a91e44cceb41ed2e7a2c516a4ed6a
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-05-14 19:00:32 +0300

    feat: add ArrowFunctionalComponentWithProps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
-import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
-import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent from "./components/LessonTwo/FunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+// import ClassComponent from "./components/LessonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LessonTwo/ArrowFunctionalComponent.tsx";
+
+// type PropsA = {
+//     title: string,
+//     description: string,
+// }
+
+// type PropsB = {
+//     title: string,
+//     description: string,
+// }
+//
+// // UNION
+// type Status = "Error" | "Warning" | "Info";
+//
+// // Tuples
+// type Coordinates = [number, number];
+//
+// //Functions
+// type ClickHandles = (event: MouseEvent) => void;
+
+//INTERSECTION
+// type Props = PropsA & PropsB;
+//
+// interface PropsTwo {
+//     title: string,
+//     description: string,
+// }
+//
+// interface  PropsTwo {
+//     price: number,
+//     sort: number,
+// }
+//PropsTwo = {title, description, price, sort}
+
+// interface PropsThree extends PropsTwo {
+//     image: string,
+// }
+//PropsThree = {title, description, price, sort, image}
 
 function App() {
 
 
   return (
     <>
-     <FunctionalComponent />
-      <ClassComponent />
-      <ArrowFunctionalComponent />
+     {/*<FunctionalComponent />*/}
+     {/*<FunctionalComponent />*/}
+     {/*<FunctionalComponent />*/}
+     {/*<FunctionalComponent />*/}
+     {/*<ClassComponent />*/}
+     {/*<ArrowFunctionalComponent />*/}
+        
+     <ArrowFunctionalComponentWithProps title="Heading 1" />
+     <ArrowFunctionalComponentWithProps
+         title="Second Title"
+         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae corporis culpa debitis iste laboriosam magnam minus perspiciatis provident quidem suscipit?" />
+
     </>
   )
 }
